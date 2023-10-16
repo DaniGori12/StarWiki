1e671301cb41a43b650c6c63232f34a929fdb28e
Dani <dani.gregori@imaginagroup.com>
2023-10-16 18:55:21 +0200

    asdf

--- src/services/fetchService.ts
+++ src/services/fetchService.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosResponse} from "axios"
 
 export const getData = async (query: string) => 
-await axios.get(`https://swapi.dev/api/${query}/?results=5/`)
+await axios.get(`https://swapi.dev/api/${query}/?/`)
 .then((response: AxiosResponse) => response)
 
 
